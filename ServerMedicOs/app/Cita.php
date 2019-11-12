a235c5f3e89fc87f21e8739529b632f7fccb512b
SaulMoreyra <armhacam21@gmail.com>
2019-11-11 22:51:15 -0600

    init server

--- ServerMedicOs/app/Cita.php
+++ ServerMedicOs/app/Cita.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cita extends Model
 {
-    //
+    //nombre de la tabla
+    protected $table = 'cita';
+
+    //llave primaria
+    protected $primaryKey = 'id_cita';
+    public $timestamps = false;
+
+    //aqui los elementos a mostrarse en la tabla 
+    protected $filltable = [
+        'id_cita',
+        'id_paciente',
+        'id_medico',
+        'fecha',
+        'hora',
+        'latitud',
+        'longitud',
+        'diagnostico',
+        'sintomas',
+        'costo',
+        'tipo_cita',
+        'status'
+    ];
 }
