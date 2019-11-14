a6656e7ea16cead4fb5e5f49b0aec3e030759412
joserios <jose_rios456@hotmail.com>
2019-11-13 19:19:20 -0600

    paciente controller

--- ServerMedicOs/app/Http/Controllers/Pacientes/PacientesController.php
+++ ServerMedicOs/app/Http/Controllers/Pacientes/PacientesController.php
@@ -6,7 +6,8 @@
     use App\Http\Controllers\Controller;
 
     use App\Models\Paciente; //Direccion de la clase (modelo)
-
+    use App\Models\Observacion;
+    use DB;
     use Illuminate\Database\QueryException;
 
     class PacientesController extends Controller{
@@ -76,8 +77,100 @@
         }
         
         public function actualizar(Request $req){
+            $status = '1';
+            $mensaje = 'ok';
+            try{
+                $paciente= Paciente::find($req->id_paciente);
+                $paciente->correo=$req->correo;
+                $paciente->curp=$req->curp;
+                $paciente->nombre=$req->nombre;
+                $paciente->primer_apellido=$req->primer_apellido;
+                $paciente->segundo_apellido=$req->segundo_apellido;
+                $paciente->fecha_nacimiento=$req->fecha_nacimiento;
+                $paciente->direccion=$req->direccion;
+                $paciente->telefono=$req->telefono;
+                $paciente->contrasena=$req->contrasena;
+                $paciente->foto_perfil=$req->foto_perfil;
+                $paciente->save();
+                
+            } catch(QueryException $ex){ 
+                $status = '0';
+                $mensaje = $ex;
+            }
+            return response()
+            ->json([
+                'status' => $status,
+                'mensaje' => $mensaje,
+                'paciente' => Paciente::select('*')->where('correo','=',$req->correo)->get()
+            ]);
+
+        }
+
+        public function antecedentesPaciente(Request $req){
+            $id_paciente=$request->id_paciente;
+            $observaciones = Observacion::select('*')->where('id_paciente',$id_paciente)->get();
+            return response()->json(['observaciones'=>$observaciones]);
+        }
+
+        public function editarAntecedente(Request $req){
+            $status = '1';
+            $mensaje = 'ok';
+            try{
+                $observacion= Observacion::find($req->id_observacion);
+                $observacion->id_observacion=$req->id_observacion;
+                $observacion->id_paciente=$req->id_paciente;
+                $observacion->tipo_obs=$req->tipo_obs;
+                $observacion->nombre_obs=$req->nombre_obs;
+                $observacion->descripcion_obs=$req->descripcion_obs;
+                $observacion->antiguedad=$req->antiguedad;
+                $observacion->save();
+                
+            } catch(QueryException $ex){ 
+                $status = '0';
+                $mensaje = $ex;
+            }
+            $observaciones = Observacion::select('*')->where('id_paciente',$req->id_paciente)->get();
+           
+       
+            return response()
+            ->json([
+                'status' => $status,
+                'mensaje' => $mensaje,
+                'observaciones' => $observaciones
+            ]);
 
 
+        }
+
+        public function newAntecedente(Request $req){
+            $status = '1';
+            $mensaje = 'ok';
+            try{
+                Observacion::insert([
+                    'id_paciente'=>$req->id_paciente,
+                    'tipo_obs'=>$req->tipo_obs,
+                    'nombre_obs'=>$req->nombre_obs,
+                    'descripcion_obs'=>$req->descripcion_obs,
+                    'antiguedad'=>$req->antiguedad
+                ]);
+            } catch(QueryException $ex){ 
+                $status = '0';
+                $mensaje = $ex;
+            }
+            $observaciones = Observacion::select('*')->where('id_paciente',$req->id_paciente)->get();
+            
+       
+            return response()
+            ->json([
+                'status' => $status,
+                'mensaje' => $mensaje,
+                'observaciones' => $observaciones
+            ]);
+
+        }
+
+        public function historialcitas(Request $req){
+            
         }
 
     }
