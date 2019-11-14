cac229ba1c1dc7f9fe8ab8a01a0569b1f25dc789
AragonMoreyraSaulRenato <armhahunter21@gmail.com>
2019-11-14 00:14:09 -0600

    init server

--- ServerMedicOs/app/Http/Models/Medico.php
+++ ServerMedicOs/app/Http/Models/Medico.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,7 +23,7 @@ class Medico extends Model
         'especialidad',
         'cedula',
         'procedencia',
-        'lalitud',
+        'latitud',
         'longitud',
         'telefono',
         'costoxconsulta',
