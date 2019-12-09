c95e0c2fcf9fcdb23ff4a250573a5d4b5a4c4d71
IgnacioLuisLuzDeisy <nissy_14@hotmail.com>
2019-12-09 17:25:23 -0600

    vista finanzas

--- MedicApp/app/src/main/java/com/emergentes/medicapp/ui/finanzas/FinanzasViewModel.java
+++ MedicApp/app/src/main/java/com/emergentes/medicapp/ui/finanzas/FinanzasViewModel.java
@@ -1,16 +1,16 @@
-package com.emergentes.medicapp.ui.share;
+package com.emergentes.medicapp.ui.finanzas;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ShareViewModel extends ViewModel {
+public class FinanzasViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public ShareViewModel() {
+    public FinanzasViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is share fragment");
+        mText.setValue("This is finanzas fragment");
     }
 
     public LiveData<String> getText() {
