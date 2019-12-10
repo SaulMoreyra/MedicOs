0fc2a95517f91966a054d776bb6c223b78169203
SaulMoreyra <armhacam21@gmail.com>
2019-12-10 01:56:57 -0600

    aplicacion 70% sin diseño

---
+++ PRUEBAS/app/src/main/java/com/example/prueba1/ui/pendientes/detallesPendientes.java
@@ -0,0 +1,16 @@
+package com.example.prueba1.ui.pendientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prueba1.R;
+
+public class detallesPendientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_pendientes);
+    }
+}
